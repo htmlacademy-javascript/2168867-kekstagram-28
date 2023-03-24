7eb194744d2d8b62609aef9da012d36cb824c303
IrinaLemeshkina <oleksinairina@gmail.com>
2023-03-24 21:46:21 +0300

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import { createDataPhotos } from './data.js';
 import { drawThumbnails } from './thumbnail.js';
+import './validator.js';
+import './open-form.js';
 
 drawThumbnails(createDataPhotos());
