a68e0d424b545275f701f9e8f9ff7dcabeaec140
IrinaLemeshkina <oleksinairina@gmail.com>
2023-03-29 14:06:04 +0300

    Задание 9 правки часть 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import { createDataPhotos } from './data.js';
 import { drawThumbnails } from './thumbnail.js';
 import './open-form.js';
-import './photo-effects.js';
-import './scale-photo.js';
 
 drawThumbnails(createDataPhotos());
