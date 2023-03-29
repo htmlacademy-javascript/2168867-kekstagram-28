b1794fa3333284bb21bfe0d7cfdba7f3491b2072
IrinaLemeshkina <oleksinairina@gmail.com>
2023-03-29 11:33:35 +0300

    Задание 9 часть 1 и 2 с правками

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 import { createDataPhotos } from './data.js';
 import { drawThumbnails } from './thumbnail.js';
-import './validator.js';
 import './open-form.js';
-import './scale-photo.js';
 import './photo-effects.js';
+import './scale-photo.js';
 
 drawThumbnails(createDataPhotos());
--- js/validator.js
+++ js/validator.js
@@ -1,88 +1,59 @@
-import { userModalHashtags, userModalComment } from './open-form.js';
-import { isValidHashtag } from './regexp.js';
-import { MAX_HASHTAG_SIMBOL_LENGTH, MAX_HASHTAG_COUNT,MAX_COMMENT_LENGTH } from './constants.js';
+import { MAX_HASHTAG_SYMBOL_LENGTH, MAX_HASHTAG_COUNT } from './constants.js';
 
-const userModalForm = document.querySelector('.img-upload__form');
-
-const hashtags = userModalHashtags;
-const userComment = userModalComment;
-
-const pristine = new Pristine(userModalForm, {
-  classTo: 'img-upload__field-wrapper',
-  errorClass: 'img-upload__field-wrapper--invalid',
-  successClass: 'img-upload__field-wrapper--valid',
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextTag: 'span',
-  errorTextClass: 'img-upload__error'
-}, false);
-
-userModalHashtags.addEventListener('focusin', () =>
-  pristine.addValidator(
-    userModalHashtags,
-    (val) => isValidHashtag.test(val),
-    'Хэштэг не соответствует правилам'
-  )
-);
-
-userModalHashtags.addEventListener('focusout', () =>
-  pristine.reset()
-);
-
-
+const getHashtag = (value) => value.trim().toLowerCase().split(' ');
 //здесь пишем условия валидации
-//нельзя указать больше пяти хэш-тегов; т.е минимальное значение 0 максимальное 5
-const validateHashtagsCount = hashtags.value.trim().length > MAX_HASHTAG_COUNT;
+const getValidator = (formElement) => {
+  const pristine = new Pristine(
+    formElement,
+    {
+      classTo: 'img-upload__field-wrapper',
+      errorClass: 'img-upload__error',
+      errorTextParent: 'img-upload__field-wrapper',
+    },
+    false
+  );
 
-pristine.addValidator(
-  userModalHashtags,
-  validateHashtagsCount,
-  'Превышено общее число хэштэгов'
-);
+  formElement.querySelectorAll('input, textarea').forEach((fieldElement) => {
+    const onInput = () => pristine.validate(fieldElement);
+    const onChange = (evt) => {
+      evt.currentTarget.addEventListener('input', onInput);
+      evt.currentTarget.removeEventListener('change', onChange);
+      pristine.validate(fieldElement);
+    };
+    fieldElement.addEventListener('change', onChange);
 
-// хештег не длинее 140 символов
-const validateHashtagsLength = () => hashtags.length > MAX_HASHTAG_SIMBOL_LENGTH;
-
-pristine.addValidator(
-  userModalHashtags,
-  validateHashtagsLength,
-  'Максимальная длина хэштэга 140 символов'
-);
-
-// хештеги не должны повторяться
-const validateUnicHashtags = () => {
-  const values = [];
-  for (let i = 0; i < hashtags.value.trim().split(' ').length; i++) {
-    const value = hashtags.value.trim().split(' ')[i];
-    if (values.indexOf(value) !== -1) {
-      return false;
+    if (fieldElement.className.includes('hashtags')) {
+      pristine.addValidator(
+        fieldElement,
+        (value) => !value || getHashtag(value).every((hashtag) => /^#/.test(hashtag)),
+        'Хэштэг должен начинаться с решетки}'
+      );
+      pristine.addValidator(
+        fieldElement,
+        (value) => !value || getHashtag(value).every((hashtag) => /[a-zа-яё0-9]{1,19}$/.test(hashtag)),
+        'Хэштэг не может содержать спецсимволы и должен содержать минимум одну букву и цифру'
+      );
+      pristine.addValidator(
+        fieldElement,
+        (value) => !value || getHashtag(value).every((hashtag) => hashtag.length <= MAX_HASHTAG_SYMBOL_LENGTH),
+        'Максимальная длина хэштэга 20 символов'
+      );
+      pristine.addValidator(
+        fieldElement,
+        (value) =>
+          getHashtag(value).length <= MAX_HASHTAG_COUNT,
+        'Превышено общее число хэштэгов'
+      );
+      pristine.addValidator(
+        fieldElement,
+        (value) => {
+          const hashtags = getHashtag(value);
+          return hashtags.length === new Set(hashtags).size;
+        },
+        'Присутствует повторяющйся хэштэг'
+      );
     }
-    values.push(value);
-  }
-  return true;
+  });
 };
 
-pristine.addValidator(
-  userModalHashtags,
-  validateUnicHashtags,
-  'Присутствует повторяющйся хэштэг'
-);
-
-const validateCommentsLength = () => userComment.length > MAX_COMMENT_LENGTH;
-
-pristine.addValidator(
-  userModalHashtags,
-  validateCommentsLength,
-  'Максимальная длина хэштэга 140 символов'
-);
-
-/*console.log(hashtags.value.length);
-userModalForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  console.log(hashtags.value.length);
-  const isValid = pristine.validate();
-  if (isValid) {
-    console.log('Можно отправлять');
-  } else {
-    console.log('Форма невалидна');
-  }
-});*/
+export { getValidator };
